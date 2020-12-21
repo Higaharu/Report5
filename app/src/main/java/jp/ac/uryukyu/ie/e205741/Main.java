3400666bc4f219d97c27bd73f883acad5b7ccec5
Higaharu <e205741@ie.u-ryukyu.ac.jp>
2020-12-21 16:02:04 +0900

    Third commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205741/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205741/Main.java
@@ -2,12 +2,14 @@ package jp.ac.uryukyu.ie.e205741;
 
 public class Main {
     public static void main (String[] args){
-        String str = null;
+        /**String str = null;
         try{
             System.out.println(str.length());
         } catch (NullPointerException n) {
             System.out.println("NullPointerException");
             System.out.println(n.getMessage());
-        }
+        }*/
+        String str = "百二十三";
+        int value =  Integer.parseInt(str);
     }
 }
