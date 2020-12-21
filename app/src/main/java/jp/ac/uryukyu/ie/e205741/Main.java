883ea878649f830c4945715654fd305e187d9230
Higaharu <e205741@ie.u-ryukyu.ac.jp>
2020-12-21 14:01:12 +0900

    Second commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205741/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205741/Main.java
@@ -2,8 +2,8 @@ package jp.ac.uryukyu.ie.e205741;
 
 public class Main {
     public static void main (String[] args){
+        String str = null;
         try{
-            String str = null;
             System.out.println(str.length());
         } catch (NullPointerException n) {
             System.out.println("NullPointerException");
